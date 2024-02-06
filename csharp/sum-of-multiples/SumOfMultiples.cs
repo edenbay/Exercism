afd05608fc685103195a09423d580fb86f3d00fd
Denniz Bayrak <deba2301@student.miun.se>
2024-02-06 17:08:27 +0100

    k

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -9,25 +9,50 @@ public static class SumOfMultiples
 
     public static IEnumerable<int> FindMultiplesUnderMax(IEnumerable<int> multiples, int max)
     {
+        IEnumerable<int> allUnderMax = from multiple in multiples
+                          where multiple += multiple < max
+                          select multiple;
 
-        var g = from multiple in multiples.TakeWhile(i => i < max)
-                where multiple += multiple
-                select multiple;
-                
+        return multiples.TakeWhile(multiple => multiples.Aggregate((multiple, multiplier) => multiple + multiplier) < max)
+                        
 
 
-        var unfilteredMultiples = from multiple in multiples
-                                  where multiple + multiple < max 
-                                  select multiple;
-    }
 
-    public static IEnumerable<int> RemoveDuplicates(IEnumerable<int> multiples, int max)
-    {
-        throw new NotImplementedException("TODO;");
-    }
 
-    private static bool IsUnderMax(this int multiple, int max)
-    {
-        return multiple < max;
     }
 }
+
+//using System;
+//using System.Collections.Generic;
+//using System.Runtime.InteropServices;
+
+//public static class SumOfMultiples
+//{
+//    public static int Sum(IEnumerable<int> multiples, int max)
+//    {
+//        var result = RemoveDuplicates(multiples, max).ToList();
+//        return result.Sum();
+//    }
+
+//    public static IEnumerable<int> FindMultiplesUnderMax(IEnumerable<int> multiples, int max)
+//    {
+//        List<int> mul = new List<int>();
+
+//        foreach (int multiple in multiples)
+//        {
+//            int num = multiple;
+//            while (num < max)
+//            {
+//                mul.Add(num);
+//                num += multiple;
+//            }
+//        }
+//        return mul;
+//    }
+
+//    public static IEnumerable<int> RemoveDuplicates(IEnumerable<int> multiples, int max)
+//    {
+//        return FindMultiplesUnderMax(multiples, max).ToList().Distinct().ToList();
+//    }
+
+//}
