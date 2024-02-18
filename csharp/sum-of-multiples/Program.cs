a21de67008d8757413f845d47e73d1dbb9985c10
edenbay <119753269+edenbay@users.noreply.github.com>
2024-02-18 16:31:07 +0100

    påbörjade lite c++

---
+++ csharp/sum-of-multiples/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penis
+{
+    static class Program
+    {
+        public static void Main(string[] args) 
+        {
+        Exercise de = new Exercise();
+          de.GetCountsOfAnimalsLegs();
+        }
+    }
+}
--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -7,27 +7,139 @@ public static class SumOfMultiples
 {
     
 
-    public static IEnumerable<int> FindMultiplesUnderMax(IEnumerable<int> multiples, int max)
+    public static int Sum(IEnumerable<int> multiples, int max)
     {
 
-        var g = from multiple in multiples.TakeWhile(i => i < max)
-                where multiple += multiple
-                select multiple;
-                
+        var sums = new List<int>();
 
+        foreach (int multiple in multiples)
+        {
+            int multiplier;
+            if (multiple.Equals(0))
+            {
+                continue;
+            }
+            else
+            {
+                 multiplier = multiple;
+            }
+            
+            while (multiplier < max)
+            {
+                sums.Add(multiplier);
+                multiplier += multiple;
+            }            
+        }
+        return sums.Distinct().Sum();
+    }
+    // x => x % x == 0
+    //public static int SumOf(IEnumerable<int> multiples, int max)
+    //{
+        
 
-        var unfilteredMultiples = from multiple in multiples
-                                  where multiple + multiple < max 
-                                  select multiple;
+        
+    //}
+    
+
+}
+namespace penis
+{
+    public class Exercise
+{
+    public List<int> GetCountsOfAnimalsLegs()
+    {
+        var animals = new List<Animal>
+            {
+                new Lion(),
+                new Tiger(),
+                new Duck(),
+                new Spider()
+            };
+
+        var result = new List<int>();
+        foreach (var animal in animals)
+        {
+            result.Add(animal.NumberOfLegs);
+        }
+        return result;
+    }
+}
+
+    p base class StringsProcessor
+    {
+        protected virtual bool IsUpperCase { get; set; } = false;
+
+        public List<string> Process(List<string> words)
+        {
+            var result = new List<string>();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                var halfOfWord = words[i].Length / 2;
+                result[i] = words[i].Substring(0, halfOfWord);
+            }
+
+            if (IsUpperCase)
+            {
+               result.ForEach(word => word.ToUpper());
+            }
+
+            return result;
+        }
     }
 
-    public static IEnumerable<int> RemoveDuplicates(IEnumerable<int> multiples, int max)
+    public class StringsProcessorcock : StringsProcessor 
     {
-        throw new NotImplementedException("TODO;");
+        protected override bool IsUpperCase { get; set; } = true;
+    }
+    
+
+
+        public abstract class Animal
+    {
+        public virtual int NumberOfLegs { get; set; } = 4;
+
+        public Animal()
+        {
+            
+        }
     }
 
-    private static bool IsUnderMax(this int multiple, int max)
+    public class Lion : Animal
     {
-        return multiple < max;
+        
+
+        public Lion()
+        {
+            
+        }
+    }
+
+    public class Tiger : Animal
+    {
+        public Tiger()
+        {
+            
+        }
+    }
+
+    public class Duck : Animal
+    {
+        public override int NumberOfLegs { get => 2; }
+
+        public Duck()
+        {
+            
+        }
+    }
+
+    public class Spider : Animal
+    {
+        public override int NumberOfLegs { get => 8; }
+
+        public Spider()
+        {
+            
+        }
     }
 }
