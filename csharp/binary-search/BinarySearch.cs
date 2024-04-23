9cd427f3697c7fd1afd2c34bf42fd9b8ed372874
edenbay <119753269+edenbay@users.noreply.github.com>
2024-04-23 15:08:43 +0200

    h

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        input.ToList().Sort();
+
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
